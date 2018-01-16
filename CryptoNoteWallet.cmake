055a5653b301e514c607c5ac1072bcc7958ff8a4
AirBitCurrencyFoundation <35303429+AirBitCurrencyFoundation@users.noreply.github.com>
2018-01-16 19:01:59 +0100

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "airbitcurrency")
+set(CN_CURRENCY_DISPLAY_NAME "AirBitCurrency")
+set(CN_CURRENCY_TICKER "AirBC")
